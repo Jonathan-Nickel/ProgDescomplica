f763ca412c1909a3da9da4dda7f35b1d0117856b
Jonathan-Nickel <jgnickel@inf.ufpel.edu.br>
2024-12-01 21:29:42 -0300

    Teste Git

---
+++ CEx1.c
@@ -0,0 +1,7 @@
+//Jonathan Goulart Nickel, 23200579 teste git prog descomplica
+#include <stdio.h>
+int main() {
+  int inteiro;
+  scanf("%d", &inteiro);
+  printf("%d\n", inteiro);
+}
---
+++ CEx2.c
@@ -0,0 +1,7 @@
+//Jonathan Goulart Nickel, 23200579 teste 2 git prog descomplica
+#include <stdio.h>
+int main() {
+  float real;
+  scanf("%f", &real);
+  printf("%.2f\n", real);
+}
